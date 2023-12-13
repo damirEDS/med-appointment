52be2cf5de7504279f8c5831f21c07c9184adf56
damirEDS <esenaevdamir123@gmail.com>
2023-12-13 14:33:36 +0600

    init

---
+++ src/main/java/com/externship/appointment/translation/controller/LanguageController.java
@@ -0,0 +1,13 @@
+package com.externship.appointment.translation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LanguageController {
+
+    @GetMapping("/ffff")
+    public String langTemplate(){
+        return "lang";
+    }
+}
