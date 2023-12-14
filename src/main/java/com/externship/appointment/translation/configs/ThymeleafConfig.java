270d253367aad2379f45cf86bb09f45cfc10fb18
damirEDS <esenaevdamir123@gmail.com>
2023-12-14 13:02:10 +0600

    package renamed

--- src/main/java/com/externship/appointment/translation/configs/ThymeleafConfig.java
+++ src/main/java/com/externship/appointment/translation/configs/ThymeleafConfig.java
@@ -1,4 +1,4 @@
-package com.externship.appointment.configs;
+package com.externship.appointment.translation.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
