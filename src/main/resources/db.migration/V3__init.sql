4dc3512650a9a8baf6d7cf38b3101d1b56caae5f
damirEDS <esenaevdamir123@gmail.com>
2023-12-14 13:01:23 +0600

    migrations

---
+++ src/main/resources/db.migration/V3__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE PERSON
+(
+    email    VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255)
+);
---
+++ src/main/resources/db.migration/V4__init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Appointment
+(
+    appId      VARCHAR(255) PRIMARY KEY,
+    email      VARCHAR(255),
+    docId      VARCHAR(255),
+    docName    VARCHAR(255),
+    docSpecial VARCHAR(255),
+    status     VARCHAR(255),
+    date       VARCHAR(255)
+);
